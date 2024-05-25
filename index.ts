73bed46a4a31179863a7d3effe86187bbd65715c
newbee1939 <hideto3970@gmail.com>
2024-05-25 17:18:19 +0900

    update

--- index.ts
+++ index.ts
@@ -1,5 +1,21 @@
+import axios from "axios";
+
 // アクセストークンの取得
-const accessToken = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml') 
+const REFRESH_TOKEN = Bun.env.REFRESH_TOKEN;
+const CLIENT_ID = Bun.env.CLIENT_ID;
+const CLIENT_SECRET = Bun.env.CLIENT_SECRET;
+
+const postData = new URLSearchParams();
+postData.append('grant_type', 'refresh_token');
+postData.append('refresh_token', REFRESH_TOKEN || "");
+postData.append('client_id', CLIENT_ID || "");
+postData.append('client_secret', CLIENT_SECRET || "");
+
+const accessToken = (await axios.post('https://accounts.spotify.com/api/token', postData, {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+})).data.access_token;
 
 // プレイリスト内の曲を全て削除
 
